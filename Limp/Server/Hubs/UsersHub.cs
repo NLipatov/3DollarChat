d1f4ef05e549d1094ba11784848fae6e0b443ad0
Nikita Lipatov <6stringsohei@gmail.com>
2022-11-25 00:38:14 +0300

    Users page is no longer making an http-request, this process is now being delegated to UsersHub.

--- Limp/Server/Hubs/UsersHub.cs
+++ Limp/Server/Hubs/UsersHub.cs
@@ -1,4 +1,5 @@
 ﻿using Limp.Server.Hubs.UserStorage;
+using Limp.Server.Utilities.HttpMessaging;
 using Limp.Shared.Models;
 using Microsoft.AspNetCore.SignalR;
 
@@ -6,28 +7,33 @@ namespace Limp.Server.Hubs
 {
     public class UsersHub : Hub
     {
+        private readonly IServerHttpClient _serverHttpClient;
+        public UsersHub(IServerHttpClient serverHttpClient)
+        {
+            _serverHttpClient = serverHttpClient;
+        }
         public async override Task OnConnectedAsync()
         {
             if (!InMemoryUsersStorage.UserConnections.Any(x => x.ConnectionIds.Contains(Context.ConnectionId)))
             {
                 InMemoryUsersStorage.UserConnections.Add(new UserConnections { ConnectionIds = new List<string>() { Context.ConnectionId } });
             }
-
-            //StaticUserStorage.TrackedUserConnections.Add(new TrackedUserConnection { ConnectionId = Context.ConnectionId });
         }
 
         public async override Task OnDisconnectedAsync(Exception? exception)
         {
             var targetConnectionId = Context.ConnectionId;
-            InMemoryUsersStorage.UserConnections.First(x=>x.ConnectionIds.Contains(targetConnectionId)).ConnectionIds.Remove(targetConnectionId);
+            InMemoryUsersStorage.UserConnections.First(x => x.ConnectionIds.Contains(targetConnectionId)).ConnectionIds.Remove(targetConnectionId);
 
             InMemoryUsersStorage.UserConnections.RemoveAll(x => x.ConnectionIds.Count == 0);
 
             await PushOnlineUsersToClients();
         }
 
-        public async Task SetUsername(string username)
+        public async Task SetUsername(string accessToken)
         {
+            var username = await _serverHttpClient.GetUserNameFromAccessTokenAsync(accessToken);
+
             if (InMemoryUsersStorage.UserConnections.Any(x => x.Username == username))
             {
                 InMemoryUsersStorage.UserConnections.First(x => x.Username == username).ConnectionIds.Add(Context.ConnectionId);

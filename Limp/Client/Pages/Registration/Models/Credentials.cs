23a84f37009ead4eeabf6eda816fc8e207e98e1f
Nikita Lipatov <6stringsohei@gmail.com>
2023-02-02 02:56:56 +0300

    Registration form validation

---
+++ Limp/Client/Pages/Registration/Models/Credentials.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Limp.Client.Pages.Registration.Models
+{
+    public class Credentials
+    {
+        [StringLength(30, MinimumLength = 3, ErrorMessage = $"Length should be in range of [3;30]")]
+        [Required(ErrorMessage = "Required")]
+        public string Username { get; set; }
+
+        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{24,}$", ErrorMessage = "Uppercase and lowecase letter, extra symbol, number and legth more than 24 characters is required.")]
+        [Required(ErrorMessage = "Required")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = $"{nameof(PasswordConfirmation)} Should match {nameof(Password)}")]
+        public string PasswordConfirmation { get; set; }
+    }
+}

882e5fa4fcbc0ef6d69483ac47d6251ee345ae93
Nikita Lipatov <6stringsohei@gmail.com>
2023-02-12 21:43:14 +0300

    Registration and logging in razor component refractoring

--- Limp/Client/UIComponents/InputForms/Registration/Modelss/RegisterFormModel.cs
+++ Limp/Client/UIComponents/InputForms/Registration/Modelss/RegisterFormModel.cs
@@ -1,16 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Limp.Client.Pages.Registration.Models
+namespace Limp.Client.UIComponents.InputForms.Registration.Models
 {
-    public class Credentials
+    public class RegisterFormModel
     {
-        [StringLength(30, MinimumLength = 3, ErrorMessage = $"{nameof(Username)} Length should be in range of [3;30]")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = $"{nameof(Username)} length should be in range of [3;30]")]
         public string Username { get; set; }
 
         [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{24,}$", ErrorMessage = "Uppercase and lowecase letter, extra symbol, number and legth more than 24 characters is required.")]
         public string Password { get; set; }
 
-        [Compare("Password", ErrorMessage = $"{nameof(PasswordConfirmation)} Should match {nameof(Password)}")]
+        [Compare("Password", ErrorMessage = $"{nameof(PasswordConfirmation)} should match {nameof(Password)}")]
         public string PasswordConfirmation { get; set; }
     }
 }

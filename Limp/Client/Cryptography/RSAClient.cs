573806641d4d091da21b615cf0551e0773dc07ee
Nikita Lipatov <6stringsohei@gmail.com>
2023-02-22 02:57:37 +0300

    Asymmetric encryption realisation

---
+++ Limp/Client/Cryptography/RSAClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography;
+
+namespace Limp.Client.Cryptography
+{
+    public class RSAClient
+    {
+        public RSA Rsa { get; set; } = RSA.Create();
+        public RSAParameters PublicParameters { get; set; }
+        private RSAParameters GetPublicParameters => Rsa.ExportParameters(false);
+    }
+}
---
+++ Limp/Client/wwwroot/js/OnClientCryptography/Encryption.js
@@ -0,0 +1,71 @@
+﻿/*
+The unwrapped signing key.
+*/
+let encryptionKey;
+
+/*
+Convert a string into an ArrayBuffer
+from https://developers.google.com/web/updates/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
+*/
+function str2ab(str) {
+    const buf = new ArrayBuffer(str.length);
+    const bufView = new Uint8Array(buf);
+    for (let i = 0, strLen = str.length; i < strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
+
+/*
+Import a PEM encoded RSA public key, to use for RSA-OAEP encryption.
+Takes a string containing the PEM encoded key, and returns a Promise
+that will resolve to a CryptoKey representing the public key.
+*/
+function importPublicKey(pem) {
+    // fetch the part of the PEM string between header and footer
+    const pemHeader = "-----BEGIN PUBLIC KEY-----";
+    const pemFooter = "-----END PUBLIC KEY-----";
+    const pemContents = pem.substring(pemHeader.length, pem.length - pemFooter.length);
+    // base64 decode the string to get the binary data
+    const binaryDerString = window.atob(pemContents);
+    // convert from a binary string to an ArrayBuffer
+    const binaryDer = str2ab(binaryDerString);
+
+    return window.crypto.subtle.importKey(
+        "spki",
+        binaryDer,
+        {
+            name: "RSA-OAEP",
+            hash: "SHA-256"
+        },
+        true,
+        ["encrypt"]
+    );
+}
+
+/*
+Get the encoded message, encrypt it and display a representation
+of the ciphertext in the "Ciphertext" element.
+*/
+async function encryptMessage(message) {
+    const encoded = new TextEncoder().encode(message);
+    const ciphertext = await window.crypto.subtle.encrypt(
+        {
+            name: "RSA-OAEP"
+        },
+        encryptionKey,
+        encoded
+    );
+
+    console.log(ciphertext);
+    return ab2str(ciphertext);
+}
+
+async function EncryptWithRSAPublicKey(message, RSApublicKey) {
+    encryptionKey = await importPublicKey(RSApublicKey);
+    return await encryptMessage(message);
+}
+
+function ab2str(buf) {
+    return String.fromCharCode.apply(null, new Uint8Array(buf));
+}

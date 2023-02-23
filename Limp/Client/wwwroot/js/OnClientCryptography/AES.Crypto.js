34cff0ffb163bbfe50b9802100a53361845485f0
Nikita Lipatov <6stringsohei@gmail.com>
2023-02-23 22:47:07 +0300

    'AES.Crypto.js' now not calls it's specific method for passing key to C#-side, but calling the same method the 'RSA.Crypto.js' is calling — 'OnKeyExtracted'.

--- Limp/Client/wwwroot/js/OnClientCryptography/AES.Crypto.js
+++ Limp/Client/wwwroot/js/OnClientCryptography/AES.Crypto.js
@@ -22,7 +22,7 @@ const exportAESKeyToDotnet = async (key) => {
     const exportedKeyBuffer = new Uint8Array(exported);
     const exportedKeyBufferString = ab2str(exportedKeyBuffer);
 
-    DotNet.invokeMethodAsync("Limp.Client", "OnAESKeyExpot", exportedKeyBufferString);
+    DotNet.invokeMethodAsync("Limp.Client", "OnKeyExtracted", exportedKeyBufferString, 3, 3);
 }
 
 function importSecretKey(ArrayBufferKeyString) {

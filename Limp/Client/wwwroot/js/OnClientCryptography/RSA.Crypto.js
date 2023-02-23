6d2672de0cb1eff5696c02114259456eb0f983d0
Nikita Lipatov <6stringsohei@gmail.com>
2023-02-23 03:45:00 +0300

    removed helper functions from RSA.Crypto, these helper functions are now in separated js file

--- Limp/Client/wwwroot/js/OnClientCryptography/RSA.Crypto.js
+++ Limp/Client/wwwroot/js/OnClientCryptography/RSA.Crypto.js
@@ -1,19 +1,4 @@
-﻿function ab2str(buf) {
-    return String.fromCharCode.apply(null, new Uint8Array(buf));
-}
-
-//Convert a string into an ArrayBuffer
-//from https://developers.google.com/web/updates/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
-function str2ab(str) {
-    const buf = new ArrayBuffer(str.length);
-    const bufView = new Uint8Array(buf);
-    for (let i = 0, strLen = str.length; i < strLen; i++) {
-        bufView[i] = str.charCodeAt(i);
-    }
-    return buf;
-}
-
-let cryptoKeyPair;
+﻿let cryptoKeyPair;
 
 function GenerateRSAOAEPKeyPair()
 {

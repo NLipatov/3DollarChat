7fc1bd008964857c898d1894392ec8966cae6495
Nikita Lipatov <6stringsohei@gmail.com>
2023-03-06 00:43:35 +0300

    Users component usersHub interaction refractoring

---
+++ Limp/Client/HubInteraction/Handlers/IHandler.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Limp.Client.HubInteraction.Handlers
+{
+    public interface IHandler<T>
+    {
+        Task<HubConnection> ConnectAsync();
+    }
+}
---
+++ Limp/Client/HubInteraction/Handlers/UsersHandler.cs
@@ -0,0 +1,50 @@
+﻿using ClientServerCommon.Models;
+using Limp.Client.HubInteraction.EventSubscriptionManager;
+using Limp.Client.HubInteraction.Handlers.Helpers;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.JSInterop;
+
+namespace Limp.Client.HubInteraction.Handlers;
+
+public class UsersHandler : IHandler<UsersHandler>
+{
+    private readonly NavigationManager _navigationManager;
+    private readonly IJSRuntime _jSRuntime;
+    private HubConnection? usersHub;
+    public UsersHandler
+        (NavigationManager navigationManager,
+        IJSRuntime jSRuntime)
+    {
+        _navigationManager = navigationManager;
+        _jSRuntime = jSRuntime;
+    }
+
+    public async Task<HubConnection> ConnectAsync()
+    {
+        usersHub = new HubConnectionBuilder()
+        .WithUrl(_navigationManager.ToAbsoluteUri("/usersHub"))
+        .Build();
+
+        usersHub.On<List<UserConnections>>("ReceiveOnlineUsers", updatedTrackedUserConnections =>
+        {
+            UsersHubSubscriptionManager.CallUsersConnectionsReceived(updatedTrackedUserConnections);
+        });
+
+        usersHub.On<string>("ReceiveConnectionId", connectionId =>
+        {
+            UsersHubSubscriptionManager.CallConnectionIdReceived(connectionId);
+        });
+
+        usersHub.On<string>("onNameResolve", username =>
+        {
+            UsersHubSubscriptionManager.CallUsernameResolved(username);
+        });
+
+        await usersHub.StartAsync();
+
+        await usersHub.SendAsync("SetUsername", await JWTHelper.GetAccessToken(_jSRuntime));
+
+        return usersHub;
+    }
+}

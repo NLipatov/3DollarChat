3545472a5a0a3b793b8d09698af41dba58d86da6
Nikita Lipatov <6stringsohei@gmail.com>
2023-04-16 13:25:56 +0300

    MessageType commentaries added

--- Limp/Shared/Models/Message/MessageType.cs
+++ Limp/Shared/Models/Message/MessageType.cs
@@ -2,7 +2,16 @@
 
 public enum MessageType
 {
+    /// <summary>
+    /// A message that user will see in GUI
+    /// </summary>
     UserMessage,
+    /// <summary>
+    /// System message containing AES key proposal
+    /// </summary>
     AESOffer,
+    /// <summary>
+    /// System message that is sent to accept AES key proposal
+    /// </summary>
     AESAccept
 }

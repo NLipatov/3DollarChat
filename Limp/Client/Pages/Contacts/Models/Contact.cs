66949b91db867785536e2cd4707f65298b4871ab
Nikita Lipatov <6stringsohei@gmail.com>
2023-05-01 18:40:04 +0300

    Contact is now a separate class

---
+++ Limp/Client/Pages/Contacts/Models/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace Limp.Client.Pages.Contacts.Models
+{
+    public class Contact
+    {
+        public string Username { get; set; } = "Contact without name";
+        public bool IsOnline { get; set; } = false;
+        public DateTime LastMessage { get; set; } = DateTime.UtcNow;
+    }
+}

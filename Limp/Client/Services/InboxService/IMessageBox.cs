dac068e3b00612e013a6c92434630a6c683e2ce4
Nikita Lipatov <6stringsohei@gmail.com>
2023-05-27 15:54:47 +0300

    clearing input box after sending a message

--- Limp/Client/Services/InboxService/IMessageBox.cs
+++ Limp/Client/Services/InboxService/IMessageBox.cs
@@ -4,6 +4,7 @@ namespace Limp.Client.Services.InboxService
 {
     public interface IMessageBox
     {
+        void MarkAsReceived(Guid messageId);
         Task AddMessageAsync(Message message, bool isEncrypted = true);
         List<Message> FetchMessagesFromMessageBox(string topic);
     }

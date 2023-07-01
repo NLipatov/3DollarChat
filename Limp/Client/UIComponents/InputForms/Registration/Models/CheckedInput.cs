7f995c591f83a3f1b87b9f792caabd2f9d02e1da
Nikita Lipatov <6stringsohei@gmail.com>
2023-07-01 18:01:52 +0300

    InputBox label property added; registration form input validation updates

--- Limp/Client/UIComponents/InputForms/Registration/Models/CheckedInput.cs
+++ Limp/Client/UIComponents/InputForms/Registration/Models/CheckedInput.cs
@@ -2,14 +2,40 @@
 {
     public record CheckedInput
     {
-        public CheckedInput(string name)
+        public CheckedInput
+            (string name,
+            Action<string, CheckedInput>? onValueSet = null)
         {
             Name = name;
+            OnValueSet = onValueSet;
         }
+        public void ValidateAgainst(CheckedInput reference)
+        {
+            IsTouched = true;
+            ErrorMessages.Clear();
+            if (Value != reference.Value)
+            {
+                IsLastValidationSucceeded = false;
+                ErrorMessages.Add("Password not matches with Password Confirmation.");
+            }
+        }
+        private Action<string, CheckedInput>? OnValueSet { get; init; }
         public string Name { get; set; } = string.Empty;
-        public string Value { get; set; } = string.Empty;
+        private string _value { get; set; } = string.Empty;
+        public string Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                IsTouched = true;
+                if (OnValueSet is not null)
+                    OnValueSet(value, this);
+            }
+        }
         public bool IsTouched { get; set; } = false;
-        public bool IsValid { get; set; } = true;
+        public bool IsLastValidationSucceeded { get; set; } = true;
+        public bool IsValid => IsTouched && IsLastValidationSucceeded;
         public List<string> ErrorMessages { get; set; } = new();
     }
 }

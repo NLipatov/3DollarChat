0e265d89bc882dfcb350871215d23de77db04195
Nikita Lipatov <6stringsohei@gmail.com>
2023-07-01 15:52:01 +0300

    Registration form validation

---
+++ Limp/Client/UIComponents/InputForms/Registration/Models/CheckedInput.cs
@@ -0,0 +1,15 @@
+﻿namespace Limp.Client.UIComponents.InputForms.Registration.Models
+{
+    public record CheckedInput
+    {
+        public CheckedInput(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+        public bool IsTouched { get; set; } = false;
+        public bool IsValid { get; set; } = true;
+        public List<string> ErrorMessages { get; set; } = new();
+    }
+}
---
+++ Limp/Client/UIComponents/InputForms/Registration/Models/RegisterFormModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Limp.Client.UIComponents.InputForms.Registration.Models
+{
+    public class RegisterFormModel
+    {
+        public CheckedInput Username { get; set; } = new("Username");
+        public CheckedInput Password { get; set; } = new("Password");
+        public CheckedInput PasswordConfirmation { get; set; } = new("PasswordConfirmation");
+    }
+}

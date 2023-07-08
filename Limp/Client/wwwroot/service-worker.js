d9481302446ff10231bc9ffa5edad069a9365e5e
Nikita Lipatov <6stringsohei@gmail.com>
2023-07-08 14:59:24 +0300

    push notifications naive implementation

--- Limp/Client/wwwroot/service-worker.js
+++ Limp/Client/wwwroot/service-worker.js
@@ -2,3 +2,26 @@
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
 self.addEventListener('fetch', () => { });
+
+self.addEventListener('install', async event => {
+    console.log('Installing service worker...');
+    self.skipWaiting();
+});
+
+self.addEventListener('push', event => {
+    console.log("On push");
+    const payload = event.data.json();
+    event.waitUntil(
+        self.registration.showNotification('3$ Chat', {
+            body: payload.message,
+            icon: 'icon-512.png',
+            vibrate: [100, 50, 100],
+            data: { url: payload.url }
+        })
+    );
+});
+
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    event.waitUntil(clients.openWindow(event.notification.data.url));
+});
--- Limp/Server/Program.cs
+++ Limp/Server/Program.cs
@@ -1,3 +1,4 @@
+using Limp.Client.Notifications;
 using Limp.Server.Extensions;
 using Limp.Server.Hubs;
 using Limp.Server.Hubs.MessageDispatcher;
@@ -6,6 +7,9 @@ using Limp.Server.Hubs.UsersConnectedManaging.EventHandling;
 using Limp.Server.Hubs.UsersConnectedManaging.EventHandling.Handlers;
 using Limp.Server.Hubs.UsersConnectedManaging.EventHandling.OnlineUsersRequestEvent;
 using Microsoft.AspNetCore.ResponseCompression;
+using Org.BouncyCastle.Asn1.X509;
+using System.Text.Json;
+using WebPush;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -60,5 +64,45 @@ app.MapHub<AuthHub>("/authHub");
 app.MapHub<UsersHub>("/usersHub");
 app.MapHub<MessageHub>("/messageDispatcherHub");
 app.MapFallbackToFile("index.html");
+
+NotificationSubscription? _subscription;
+// Subscribe to notifications
+app.MapPut("/notifications/subscribe", async (
+    HttpContext context,
+    NotificationSubscription subscription) => {
+        _subscription = subscription;
+        SendNotification();
+        return Results.Ok(subscription);
+    });
+
+async void SendNotification()
+{
+    await SendNotificationAsync($"Пожилое уведомление");
+}
+
+async Task SendNotificationAsync(string message)
+{
+    // For a real application, generate your own
+    var publicKey = "BLC8GOevpcpjQiLkO7JmVClQjycvTCYWm6Cq_a7wJZlstGTVZvwGFFHMYfXt6Njyvgx_GlXJeo5cSiZ1y4JOx1o";
+    var privateKey = "OrubzSz3yWACscZXjFQrrtDwCKg-TGFuWhluQ2wLXDo";
+
+    var pushSubscription = new PushSubscription(_subscription.Url, _subscription.P256dh, _subscription.Auth);
+    var vapidDetails = new VapidDetails("mailto:<someone@example.com>", publicKey, privateKey);
+    var webPushClient = new WebPushClient();
+    try
+    {
+        var payload = JsonSerializer.Serialize(new
+        {
+            message,
+            //This will redirect user to specified url
+            url = $"/Contacts",
+        });
+        await webPushClient.SendNotificationAsync(pushSubscription, payload, vapidDetails);
+    }
+    catch (Exception ex)
+    {
+        Console.Error.WriteLine("Error sending push notification: " + ex.Message);
+    }
+}
 
 app.Run();

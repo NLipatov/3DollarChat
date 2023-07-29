821becbc1291e17450dc846b6c3cc1995133c990
Nikita Lipatov <6stringsohei@gmail.com>
2023-07-29 13:23:44 +0300

    ContactList replaced by generic EditableList

---
+++ Limp/Client/Shared/ReuseableComponents/EditableList/ItemModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Limp.Client.Shared.ReuseableComponents.EditableList
+{
+    public class ItemModel
+    {
+        public string ItemName { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = false;
+        public bool IsEncryptionSettedUp { get; set; } = false;
+    }
+}

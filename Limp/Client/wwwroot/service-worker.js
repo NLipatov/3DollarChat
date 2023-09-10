1ec53c64ffdf717e2133f2eb38b1c06dda5bd84d
Nikita Lipatov <6stringsohei@gmail.com>
2023-09-10 19:32:46 +0300

    service-worker.js update

--- Limp/Client/wwwroot/service-worker.js
+++ Limp/Client/wwwroot/service-worker.js
@@ -10,7 +10,10 @@ self.addEventListener('install', async event => {
 
 self.addEventListener("push", function(e){
     event.waitUntil(
-        self.registration.showNotification(e.data.title, e.data)
+        self.registration.showNotification(e.data.title, {
+            tag: generateUUID().toString(),
+            body: "You've got a new message!"
+        })
     );
 });
 
@@ -18,3 +21,19 @@ self.addEventListener('notificationclick', event => {
     event.notification.close();
     event.waitUntil(clients.openWindow(event.notification.data.url));
 });
+
+function generateUUID() { // Public Domain/MIT
+    var d = new Date().getTime();//Timestamp
+    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}

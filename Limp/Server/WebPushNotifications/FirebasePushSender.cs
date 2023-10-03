dab8c2cac2c8c17e999b40914dce4e00f99d5829
Nikita Lipatov <6stringsohei@gmail.com>
2023-10-03 21:12:22 +0300

    sending pushes to all of the subscriptions in parallel

--- Limp/Server/WebPushNotifications/FirebasePushSender.cs
+++ Limp/Server/WebPushNotifications/FirebasePushSender.cs
@@ -2,6 +2,7 @@ using FirebaseAdmin;
 using FirebaseAdmin.Messaging;
 using Google.Apis.Auth.OAuth2;
 using Limp.Server.Utilities.HttpMessaging;
+using LimpShared.Models.WebPushNotification;
 
 namespace Limp.Server.WebPushNotifications;
 
@@ -29,34 +30,50 @@ public class FirebasePushSender : IWebPushSender
         
             if (!subscriptions.Any())
                 return;
-        
-            var notificationMessage = new Message()
-            {
-                Notification = new Notification()
-                {
-                    Title = "Etha Chat",
-                    Body = pushBodyText
-                },
-                Token = subscriptions.First().FirebaseRegistrationToken
-            };
-        
-            if (FirebaseApp.DefaultInstance == null)
-            {
-                var options = new AppOptions()
-                {
-                    Credential = GoogleCredential.FromFile(_configurationPath)
-                };
 
-                FirebaseApp.Create(options);
-            }
-        
-            var messaging = FirebaseMessaging.DefaultInstance;
-            await messaging.SendAsync(notificationMessage);
-
+            var sendPushesWorkload = CreateSendPushesWorkload(subscriptions, pushBodyText);
+            await Task.WhenAll(sendPushesWorkload);
         }
         catch (Exception e)
         {
             throw new ApplicationException($"Cannot send a push {e.Message}.");
         }
+    }
+
+    private Task[] CreateSendPushesWorkload(NotificationSubscriptionDto[] subscriptions, string pushBodyText)
+    {
+        Task[] workload = new Task[subscriptions.Length];
+        for (int i = 0; i < subscriptions.Length; i++)
+        {
+            int index = i; // Сделать копию значения i
+
+            workload[index] = Task.Run(async () =>
+            {
+                var notificationMessage = new Message()
+                {
+                    Notification = new Notification()
+                    {
+                        Title = "Etha Chat",
+                        Body = pushBodyText
+                    },
+                    Token = subscriptions[index].FirebaseRegistrationToken // Использовать скопированный index
+                };
+
+                if (FirebaseApp.DefaultInstance == null)
+                {
+                    var options = new AppOptions()
+                    {
+                        Credential = GoogleCredential.FromFile(_configurationPath)
+                    };
+
+                    FirebaseApp.Create(options);
+                }
+
+                var messaging = FirebaseMessaging.DefaultInstance;
+                await messaging.SendAsync(notificationMessage);
+            });
+        }
+
+        return workload;
     }
 }

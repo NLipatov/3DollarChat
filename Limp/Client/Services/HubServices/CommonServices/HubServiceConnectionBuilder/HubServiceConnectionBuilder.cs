f5785e049095fcac54afc802772e3628477828ca
Nikita Lipatov <6stringsohei@gmail.com>
2024-01-03 18:57:07 +0400

    removed unneeded try catch

--- Limp/Client/Services/HubServices/CommonServices/HubServiceConnectionBuilder/HubServiceConnectionBuilder.cs
+++ Limp/Client/Services/HubServices/CommonServices/HubServiceConnectionBuilder/HubServiceConnectionBuilder.cs
@@ -4,26 +4,9 @@ namespace Ethachat.Client.Services.HubServices.CommonServices.HubServiceConnecti
 
 public static class HubServiceConnectionBuilder
 {
-    public static HubConnection Build(Uri hubAddress, bool useStatefulReconnect = true)
-    {
-        Console.WriteLine($"{nameof(HubServiceConnectionBuilder)}.{nameof(Build)}: Building hub connection at {hubAddress}");
-        HubConnection hubConnection;
-        try
-        {
-            hubConnection = new HubConnectionBuilder()
-                .WithUrl(hubAddress, options =>
-                {
-                    options.UseStatefulReconnect = useStatefulReconnect;
-                })
-                .AddMessagePackProtocol()
-                .Build();
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            throw;
-        }
-
-        return hubConnection;
-    }
+    public static HubConnection Build(Uri hubAddress, bool useStatefulReconnect = true) 
+        => new HubConnectionBuilder()
+        .WithUrl(hubAddress, options => { options.UseStatefulReconnect = useStatefulReconnect; })
+        .AddMessagePackProtocol()
+        .Build();
 }

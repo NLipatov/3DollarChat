2cce4f0a04dfdbea042c1d4500369a2c55ed9a75
Nikita Lipatov <6stringsohei@gmail.com>
2024-01-04 02:04:28 +0400

    moved split method logic to the PackageMultiplexerService.cs

--- Limp/Client/Services/DataTransmission/PackageForming/IPackageMultiplexerService.cs
+++ Limp/Client/Services/DataTransmission/PackageForming/IPackageMultiplexerService.cs
@@ -1,8 +1,10 @@
 ﻿using Ethachat.Client.ClientOnlyModels;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace Ethachat.Client.Services.DataTransmission.PackageForming;
 
 public interface IPackageMultiplexerService
 {
     string Combine(List<ClientPackage> packages, string partnerUsername);
+    Task<List<string>> SplitAsync(IBrowserFile file);
 }

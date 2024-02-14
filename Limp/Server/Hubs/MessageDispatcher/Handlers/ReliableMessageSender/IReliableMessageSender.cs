db521ffb45d206534a10ac872a38d731aaf68713
Nikita Lipatov <6stringsohei@gmail.com>
2024-02-15 00:15:06 +0400

    IReliableMessageSender interface

--- Limp/Server/Hubs/MessageDispatcher/Handlers/ReliableMessageSender/IReliableMessageSender.cs
+++ Limp/Server/Hubs/MessageDispatcher/Handlers/ReliableMessageSender/IReliableMessageSender.cs
@@ -4,6 +4,6 @@ namespace Ethachat.Server.Hubs.MessageDispatcher.Handlers.ReliableMessageSender;
 
 public interface IReliableMessageSender
 {
-    void Enqueue(Message message);
-    void OnAckReceived(Guid messageId, string targetGroup);
+    Task Enqueue(Message message);
+    void OnAckReceived(Message syncMessage);
 }

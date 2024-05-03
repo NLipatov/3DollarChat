9d6955dfc97a1f680d28fc530d883ccc97c86068
Nikita Lipatov <6stringsohei@gmail.com>
2024-05-03 20:14:07 +0300

    removed unused code

--- Limp/Client/Services/NotificationService/Implementation/WebPushService.cs
+++ Limp/Client/Services/NotificationService/Implementation/WebPushService.cs
@@ -1,11 +1,8 @@
-﻿using Ethachat.Client.Services.AuthenticationService.Handlers;
-using Ethachat.Client.Services.HubServices.HubServices.Implementations.UsersService;
+﻿using Ethachat.Client.Services.HubServices.HubServices.Implementations.UsersService;
 using Ethachat.Client.Services.LocalStorageService;
-using Ethachat.Client.Services.NotificationService.Implementation.Types;
 using EthachatShared.Models.Authentication.Models.Credentials;
 using EthachatShared.Models.Authentication.Models.Credentials.Implementation;
 using EthachatShared.Models.WebPushNotification;
-using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 
 namespace Ethachat.Client.Services.NotificationService.Implementation
@@ -15,21 +12,15 @@ namespace Ethachat.Client.Services.NotificationService.Implementation
         private readonly IJSRuntime _jSRuntime;
         private readonly IUsersService _usersService;
         private readonly ILocalStorageService _localStorageService;
-        private readonly NavigationManager _navigationManager;
-        private readonly IAuthenticationHandler _authenticationHandler;
 
         public WebPushService
             (IJSRuntime jSRuntime, 
                 IUsersService usersService, 
-                ILocalStorageService localStorageService,
-                NavigationManager navigationManager,
-                IAuthenticationHandler authenticationHandler)
+                ILocalStorageService localStorageService)
         {
             _jSRuntime = jSRuntime;
             _usersService = usersService;
             _localStorageService = localStorageService;
-            _navigationManager = navigationManager;
-            _authenticationHandler = authenticationHandler;
         }
 
         public async Task RequestWebPushPermission(ICredentials credentials)
@@ -49,20 +40,6 @@ namespace Ethachat.Client.Services.NotificationService.Implementation
             };
             
             await _usersService.AddUserWebPushSubscription(subscription);
-        }
-
-        private async Task<PushPermissionType> IsWebPushPermissionGranted()
-        {
-            string permissionTypeString = await _jSRuntime
-                .InvokeAsync<string>("eval", "navigator.permissions.query({ name: 'push', userVisibleOnly: true }).then(result => result.state)");
-
-            Console.WriteLine("permission:" + permissionTypeString);
-            bool isTypeParsed = Enum.TryParse(permissionTypeString, true, out PushPermissionType parsedType);
-
-            if (isTypeParsed)
-                return parsedType;
-            else
-                return PushPermissionType.UNKNOWN;
         }
 
         public async Task ResetWebPushPermission()

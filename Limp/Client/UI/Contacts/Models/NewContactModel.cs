f1adac0b771d2257b8dd7f50464924168da96a0e
Nikita Lipatov <6stringsohei@gmail.com>
2024-06-07 19:00:35 +0300

    Confirm contact addition modal refactoring

---
+++ Limp/Client/UI/Contacts/Models/NewContactModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Ethachat.Client.UI.Contacts.Models
+{
+    public record class NewContactModel
+    {
+        public bool Exists { get; set; } = false;
+        public string Username { get; set; } = string.Empty;
+    }
+}

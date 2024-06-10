155dbcd7a641dbd5d59fb3442bd9de854cef0786
Nikita Lipatov <6stringsohei@gmail.com>
2024-06-10 16:37:16 +0300

    filescoped AesHandler.cs

--- Client.Infrastructure.Cryptography/Handlers/AesHandler.cs
+++ Client.Infrastructure.Cryptography/Handlers/AesHandler.cs
@@ -3,42 +3,41 @@ using Client.Infrastructure.Cryptography.Handlers.Models;
 using EthachatShared.Encryption;
 using EthachatShared.Models.Message;
 
-namespace Client.Infrastructure.Cryptography.Handlers
+namespace Client.Infrastructure.Cryptography.Handlers;
+
+public class AesHandler(IRuntimeCryptographyExecutor runtimeCryptographyExecutor) : ICryptoHandler
 {
-    public class AesHandler(IRuntimeCryptographyExecutor runtimeCryptographyExecutor) : ICryptoHandler
+    public async Task<Cryptogram> Decrypt(Cryptogram cryptogram, Key key)
     {
-        public async Task<Cryptogram> Decrypt(Cryptogram cryptogram, Key key)
+        EncryptionResult result = await runtimeCryptographyExecutor.InvokeAsync<EncryptionResult>("AESDecryptText",
+        [
+            cryptogram.Cyphertext ?? string.Empty,
+            key.Value?.ToString() ?? throw new ApplicationException("Missing key"),
+            cryptogram.Iv
+        ]);
+
+        return new()
         {
-            EncryptionResult result = await runtimeCryptographyExecutor.InvokeAsync<EncryptionResult>("AESDecryptText",
+            Cyphertext = result.Ciphertext,
+            Iv = result.Iv,
+            KeyId = key.Id
+        };
+    }
+
+    public async Task<Cryptogram> Encrypt(Cryptogram cryptogram, Key key)
+    {
+        EncryptionResult result = await runtimeCryptographyExecutor
+            .InvokeAsync<EncryptionResult>("AESEncryptText",
             [
                 cryptogram.Cyphertext ?? string.Empty,
-                key.Value?.ToString() ?? throw new ApplicationException("Missing key"),
-                cryptogram.Iv
+                key.Value?.ToString() ?? throw new ApplicationException("Missing key")
             ]);
 
-            return new()
-            {
-                Cyphertext = result.Ciphertext,
-                Iv = result.Iv,
-                KeyId = key.Id
-            };
-        }
-
-        public async Task<Cryptogram> Encrypt(Cryptogram cryptogram, Key key)
+        return new()
         {
-            EncryptionResult result = await runtimeCryptographyExecutor
-                .InvokeAsync<EncryptionResult>("AESEncryptText",
-                [
-                    cryptogram.Cyphertext ?? string.Empty,
-                    key.Value?.ToString() ?? throw new ApplicationException("Missing key")
-                ]);
-
-            return new()
-            {
-                Cyphertext = result.Ciphertext,
-                Iv = result.Iv,
-                KeyId = key.Id,
-            };
-        }
+            Cyphertext = result.Ciphertext,
+            Iv = result.Iv,
+            KeyId = key.Id,
+        };
     }
 }

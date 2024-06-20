60752800e9312929ee8ed4ac8d943ccf3ffd7275
Nikita Lipatov <6stringsohei@gmail.com>
2024-06-20 19:53:21 +0300

    Removed IReliableBinaryMessageSender

--- src/Server/Hubs/MessageDispatcher/Handlers/ReliableMessageSender/ConcreteSenders/SenderImplementations/Binary/IReliableBinaryMessageSender.cs
+++
@@ -1,6 +0,0 @@
-using EthachatShared.Models.Message;
-
-namespace Ethachat.Server.Hubs.MessageDispatcher.Handlers.ReliableMessageSender.ConcreteSenders.SenderImplementations.Binary;
-
-public interface IReliableBinaryMessageSender : IReliableMessageSender<Message>
-{}
--- src/Server/Hubs/MessageDispatcher/Handlers/ReliableMessageSender/Implementation/ReliableMessageSender.cs
+++ src/Server/Hubs/MessageDispatcher/Handlers/ReliableMessageSender/Implementation/ReliableMessageSender.cs
@@ -1,7 +1,5 @@
 using Ethachat.Server.Hubs.MessageDispatcher.Handlers.MessageTransmitionGateway;
 using Ethachat.Server.Hubs.MessageDispatcher.Handlers.ReliableMessageSender.ConcreteSenders.LongTermMessageStorage;
-using Ethachat.Server.Hubs.MessageDispatcher.Handlers.ReliableMessageSender.ConcreteSenders.SenderImplementations.Binary;
-using Ethachat.Server.Hubs.MessageDispatcher.Handlers.ReliableMessageSender.ConcreteSenders.SenderImplementations.Binary.Implementation;
 using Ethachat.Server.Hubs.MessageDispatcher.Handlers.ReliableMessageSender.ConcreteSenders.SenderImplementations.Text;
 using Ethachat.Server.Hubs.MessageDispatcher.Handlers.ReliableMessageSender.ConcreteSenders.SenderImplementations.Text.Implementation;
 using EthachatShared.Models.Message;
@@ -11,18 +9,12 @@ namespace Ethachat.Server.Hubs.MessageDispatcher.Handlers.ReliableMessageSender.
 public class ReliableMessageSender : IReliableMessageSender<Message>
 {
     private static IReliableTextMessageSender _reliableTextMessageSender;
-    private static IReliableBinaryMessageSender _reliableBinaryMessageSender;
 
     public ReliableMessageSender(IMessageGateway<Message> gateway, ILongTermStorageService<Message> longTermStorageService)
     {
         if (_reliableTextMessageSender is null)
         {
             _reliableTextMessageSender = new ReliableTextMessageSender(gateway, longTermStorageService);
-        }
-
-        if (_reliableBinaryMessageSender is null)
-        {
-            _reliableBinaryMessageSender = new ReliableBinaryMessageSender(gateway, longTermStorageService);
         }
     }
 
@@ -48,9 +40,8 @@ public class ReliableMessageSender : IReliableMessageSender<Message>
     private IReliableMessageSender<Message> GetTargetSender(MessageType type)
     {
         return type switch
-        {
             
-            MessageType.Metadata or MessageType.DataPackage => _reliableBinaryMessageSender,
+        {
             _ => _reliableTextMessageSender
         };
     }

1211a674bedc2992e5145b591bc43a04ad3532bc
Nikita <6stringsohei@gmail.com>
2024-08-02 13:33:43 +0300

    Commentaries added for HlsStrategy.cs and TextStrategy.cs

--- src/Server/Services/Notifications/WebPush/PushDescriptionGeneration/Strategies/Implemetations/HlsStrategy.cs
+++ src/Server/Services/Notifications/WebPush/PushDescriptionGeneration/Strategies/Implemetations/HlsStrategy.cs
@@ -1,15 +1,19 @@
+using Ethachat.Client.ClientOnlyModels;
 using Ethachat.Server.Services.Notifications.WebPush.Commands;
 using EthachatShared.Models.Message.Interfaces;
 
 namespace Ethachat.Server.Services.Notifications.WebPush.PushDescriptionGeneration.Strategies.Implemetations;
 
+/// <summary>
+/// Generates push text message for <see cref="HlsPlaylistMessage"/>
+/// </summary>
 public class HlsStrategy : IPushItemMessageStrategy
 {
     public SendNotificationCommand CreateCommand<T>(T clientMessage) where T : IDestinationResolvable, ISourceResolvable
     {
         return new SendNotificationCommand
         {
-            PushMessage = $"You've got a new video message from {clientMessage.Sender}",
+            PushMessage = $"Video from {clientMessage.Sender}",
             IsSendRequired = true
         };
     }
--- src/Server/Services/Notifications/WebPush/PushDescriptionGeneration/Strategies/Implemetations/TextStrategy.cs
+++ src/Server/Services/Notifications/WebPush/PushDescriptionGeneration/Strategies/Implemetations/TextStrategy.cs
@@ -1,15 +1,19 @@
 using Ethachat.Server.Services.Notifications.WebPush.Commands;
+using EthachatShared.Models.Message.ClientToClientTransferData;
 using EthachatShared.Models.Message.Interfaces;
 
 namespace Ethachat.Server.Services.Notifications.WebPush.PushDescriptionGeneration.Strategies.Implemetations;
 
+/// <summary>
+/// Generates push text message for <see cref="TextMessage"/>
+/// </summary>
 public class TextStrategy : IPushItemMessageStrategy
 {
     public SendNotificationCommand CreateCommand<T>(T clientMessage) where T : IDestinationResolvable, ISourceResolvable
     {
         return new SendNotificationCommand
         {
-            PushMessage = $"You've got a new text message from {clientMessage.Sender}",
+            PushMessage = $"Text message from {clientMessage.Sender}",
             IsSendRequired = true
         };
     }

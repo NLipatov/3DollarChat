f1db121444ef7e3d7d0c3287a4ffc2ea1f88b0b0
Nikita <6stringsohei@gmail.com>
2024-08-02 20:38:48 +0300

    Runtime moved to a separate project

--- src/Client/Infrastructure/Client.Infrastructure.Runtime/PlatformRuntime/JSPlatformRuntime.cs
+++ src/Client/Infrastructure/Client.Infrastructure.Runtime/PlatformRuntime/JSPlatformRuntime.cs
@@ -2,7 +2,7 @@ using System.Diagnostics.CodeAnalysis;
 using Client.Infrastructure.Cryptography;
 using Microsoft.JSInterop;
 
-namespace Ethachat.Client.PlatformRuntime;
+namespace Client.Infrastructure.Runtime.PlatformRuntime;
 
 public class JsPlatformRuntime(IJSRuntime jsRuntime) : IPlatformRuntime
 {

842243a7e191cdfee85518671e986dcf7ce70496
Nikita <6stringsohei@gmail.com>
2024-08-19 19:22:55 +0300

    UsersService.cs now uses IGateway.cs

--- src/Client/Presentation/Services/HubServices/HubServices/Implementations/UsersService/Implementation/UsersService.cs
+++ src/Client/Presentation/Services/HubServices/HubServices/Implementations/UsersService/Implementation/UsersService.cs
@@ -1,13 +1,10 @@
 ﻿using System.Collections.Concurrent;
-using Client.Application.Cryptography.KeyStorage;
+using Client.Application.Gateway;
+using Client.Infrastructure.Gateway;
 using Ethachat.Client.Services.AuthenticationService.Handlers;
 using Ethachat.Client.Services.HubServices.CommonServices.CallbackExecutor;
-using Ethachat.Client.Services.HubServices.HubServices.Builders;
-using Ethachat.Client.Services.UserIdentityService;
 using EthachatShared.Constants;
-using EthachatShared.Encryption;
 using EthachatShared.Models.Authentication.Models.Credentials.CredentialsDTO;
-using EthachatShared.Models.Authentication.Models.Credentials.Implementation;
 using EthachatShared.Models.ConnectedUsersManaging;
 using EthachatShared.Models.Users;
 using EthachatShared.Models.WebPushNotification;
@@ -21,99 +18,30 @@ namespace Ethachat.Client.Services.HubServices.HubServices.Implementations.Users
         public NavigationManager NavigationManager { get; set; }
         private readonly ICallbackExecutor _callbackExecutor;
         private readonly IAuthenticationHandler _authenticationHandler;
-        private readonly IConfiguration _configuration;
-        private readonly IKeyStorage _keyStorage;
         private bool _isConnectionClosedCallbackSet = false;
-        private HubConnection? HubConnectionInstance { get; set; }
 
         private ConcurrentDictionary<Guid, Func<string, Task>> ConnectionIdReceivedCallbacks = new();
         private ConcurrentDictionary<Guid, Func<string, Task>> UsernameResolvedCallbacks = new();
+        private IGateway? _gateway;
+
+        private async Task<IGateway> ConfigureGateway()
+        {
+            var gateway = new SignalRGateway();
+            await gateway.ConfigureAsync(NavigationManager.ToAbsoluteUri(HubAddress.Users),
+                async () => await _authenticationHandler.GetCredentialsDto());
+            return gateway;
+        }
 
         public UsersService
         (NavigationManager navigationManager,
             ICallbackExecutor callbackExecutor,
-            IAuthenticationHandler authenticationHandler,
-            IConfiguration configuration,
-            IKeyStorage keyStorage)
+            IAuthenticationHandler authenticationHandler)
         {
             NavigationManager = navigationManager;
             _callbackExecutor = callbackExecutor;
             _authenticationHandler = authenticationHandler;
-            _configuration = configuration;
-            _keyStorage = keyStorage;
-            InitializeHubConnection();
-            RegisterHubEventHandlers();
         }
 
-        private void InitializeHubConnection()
-        {
-            if (HubConnectionInstance is not null)
-                return;
-
-            HubConnectionInstance = HubServiceConnectionBuilder
-                .Build(NavigationManager.ToAbsoluteUri(HubAddress.Users));
-        }
-
-        private void RegisterHubEventHandlers()
-        {
-            if (HubConnectionInstance is null)
-                throw new NullReferenceException($"Could not register event handlers - hub was null.");
-
-            HubConnectionInstance.On<UserConnectionsReport>("ReceiveOnlineUsers",
-                updatedTrackedUserConnections =>
-                {
-                    _callbackExecutor.ExecuteSubscriptionsByName(updatedTrackedUserConnections, "ReceiveOnlineUsers");
-                });
-
-            HubConnectionInstance.On<string>("ReceiveConnectionId",
-                connectionId =>
-                {
-                    _callbackExecutor.ExecuteCallbackDictionary(connectionId, ConnectionIdReceivedCallbacks);
-                });
-
-            HubConnectionInstance.On<string>("OnNameResolve", async username =>
-            {
-                ActiveUserIdentity.SetUsername(username);
-                
-                _callbackExecutor.ExecuteSubscriptionsByName(username, "OnNameResolve");
-                
-                _callbackExecutor.ExecuteCallbackDictionary(username, UsernameResolvedCallbacks);
-
-                await GetHubConnectionAsync();
-
-                var rsaPublicKeys = await _keyStorage.GetAsync(string.Empty, KeyType.RsaPublic);
-                await HubConnectionInstance.SendAsync("PostAnRSAPublic", username,
-                    rsaPublicKeys.First().Value);
-            });
-
-            HubConnectionInstance.On<UserConnection>("IsUserOnlineResponse",
-                (UserConnection) =>
-                {
-                    _callbackExecutor.ExecuteSubscriptionsByName(UserConnection, "IsUserOnlineResponse");
-                });
-
-            HubConnectionInstance.On<NotificationSubscriptionDto[]>("ReceiveWebPushSubscriptions",
-                subscriptions =>
-                {
-                    _callbackExecutor.ExecuteSubscriptionsByName(subscriptions, "ReceiveWebPushSubscriptions");
-                });
-
-            HubConnectionInstance.On<NotificationSubscriptionDto[]>("RemovedFromWebPushSubscriptions",
-                removedSubscriptions =>
-                {
-                    _callbackExecutor.ExecuteSubscriptionsByName(removedSubscriptions,
-                        "RemovedFromWebPushSubscriptions");
-                });
-
-            HubConnectionInstance.On("WebPushSubscriptionSetChanged",
-                () => { _callbackExecutor.ExecuteSubscriptionsByName("WebPushSubscriptionSetChanged"); });
-
-            HubConnectionInstance.On<IsUserExistDto>("UserExistanceResponse",
-                async isUserExistDTO =>
-                {
-                    _callbackExecutor.ExecuteSubscriptionsByName(isUserExistDTO, "UserExistanceResponse");
-                });
-        }
 
         public async Task<HubConnection> GetHubConnectionAsync()
         {
@@ -122,122 +50,94 @@ namespace Ethachat.Client.Services.HubServices.HubServices.Implementations.Users
                 NavigationManager.NavigateTo("signin");
             }
 
-            if (HubConnectionInstance == null)
-                throw new ArgumentException($"{nameof(HubConnectionInstance)} was not properly instantiated.");
+            await InitializeGatewayAsync();
+            return null;
+        }
 
-            while (HubConnectionInstance.State is HubConnectionState.Disconnected)
-            {
-                try
+        private async Task InitializeGatewayAsync()
+        {
+            _gateway ??= await ConfigureGateway();
+
+            await _gateway.AddEventCallbackAsync<UserConnectionsReport>("ReceiveOnlineUsers",
+                updatedTrackedUserConnections =>
                 {
-                    await HubConnectionInstance.StartAsync();
-                }
-                catch
+                    _callbackExecutor.ExecuteSubscriptionsByName(updatedTrackedUserConnections, "ReceiveOnlineUsers");
+                    return Task.CompletedTask;
+                });
+
+            await _gateway.AddEventCallbackAsync<string>("ReceiveConnectionId",
+                connectionId =>
                 {
-                    var interval = int.Parse(_configuration["HubConnection:ReconnectionIntervalMs"] ?? "0");
-                    await Task.Delay(interval);
-                    return await GetHubConnectionAsync();
-                }
-            }
-            
-            _callbackExecutor.ExecuteSubscriptionsByName(true, "OnUsersHubConnectionStatusChanged");
-            
-            await HubConnectionInstance.SendAsync("SetUsername", await _authenticationHandler.GetCredentialsDto());
+                    _callbackExecutor.ExecuteCallbackDictionary(connectionId, ConnectionIdReceivedCallbacks);
+                    return Task.CompletedTask;
+                });
 
-            if (_isConnectionClosedCallbackSet is false)
+            await _gateway.AddEventCallbackAsync<string>("OnNameResolve", async username =>
             {
-                HubConnectionInstance.Closed += OnConnectionLost;
-                _isConnectionClosedCallbackSet = true;
-            }
+                _callbackExecutor.ExecuteSubscriptionsByName(username, "OnNameResolve");
 
-            return HubConnectionInstance;
-        }
-        
-        private async Task OnConnectionLost(Exception? exception)
-        {
-            _callbackExecutor.ExecuteSubscriptionsByName(false, "OnUsersHubConnectionStatusChanged");
-            await GetHubConnectionAsync();
-        }
+                _callbackExecutor.ExecuteCallbackDictionary(username, UsernameResolvedCallbacks);
+            });
 
-        public void RemoveConnectionIdReceived(Guid subscriptionId)
-        {
-            bool isRemoved = ConnectionIdReceivedCallbacks.Remove(subscriptionId, out _);
-            if (!isRemoved)
-            {
-                RemoveConnectionIdReceived(subscriptionId);
-            }
-        }
+            await _gateway.AddEventCallbackAsync<UserConnection>("IsUserOnlineResponse",
+                (UserConnection) =>
+                {
+                    _callbackExecutor.ExecuteSubscriptionsByName(UserConnection, "IsUserOnlineResponse");
+                    return Task.CompletedTask;
+                });
 
-        public Guid SubscribeToConnectionIdReceived(Func<string, Task> callback)
-        {
-            Guid subscriptionId = Guid.NewGuid();
-            bool isAdded = ConnectionIdReceivedCallbacks.TryAdd(subscriptionId, callback);
-            if (!isAdded)
-            {
-                SubscribeToConnectionIdReceived(callback);
-            }
+            await _gateway.AddEventCallbackAsync<NotificationSubscriptionDto[]>("ReceiveWebPushSubscriptions",
+                subscriptions =>
+                {
+                    _callbackExecutor.ExecuteSubscriptionsByName(subscriptions, "ReceiveWebPushSubscriptions");
+                    return Task.CompletedTask;
+                });
 
-            return subscriptionId;
-        }
+            await _gateway.AddEventCallbackAsync<NotificationSubscriptionDto[]>("RemovedFromWebPushSubscriptions",
+                removedSubscriptions =>
+                {
+                    _callbackExecutor.ExecuteSubscriptionsByName(removedSubscriptions,
+                        "RemovedFromWebPushSubscriptions");
+                    
+                    return Task.CompletedTask;
+                });
 
-        public Guid SubscribeToUsernameResolved(Func<string, Task> callback)
-        {
-            Guid subscriptionId = Guid.NewGuid();
-            bool isAdded = UsernameResolvedCallbacks.TryAdd(subscriptionId, callback);
-            if (!isAdded)
-            {
-                SubscribeToUsernameResolved(callback);
-            }
+            await _gateway.AddEventCallbackAsync("WebPushSubscriptionSetChanged",
+                () => 
+                {
+                    _callbackExecutor.ExecuteSubscriptionsByName("WebPushSubscriptionSetChanged");
+                    return Task.CompletedTask;
+                });
 
-            return subscriptionId;
-        }
-
-        public void RemoveUsernameResolved(Guid subscriptionId)
-        {
-            bool isRemoved = UsernameResolvedCallbacks.Remove(subscriptionId, out _);
-            if (!isRemoved)
-            {
-                RemoveUsernameResolved(subscriptionId);
-            }
-        }
-
-        public async Task SetRSAPublicKey(Key RSAPublicKey)
-        {
-            var rsaPublicKey = RSAPublicKey.Value?.ToString() ?? throw new ArgumentException("Invalid key");
-            var credentials = await _authenticationHandler.GetCredentials();
-            if (credentials is WebAuthnPair)
-            {
-                var hubConnection = await GetHubConnectionAsync();
-                await hubConnection.SendAsync("SetRSAPublicKey", RSAPublicKey, credentials, null);
-            }
-            else if (credentials is JwtPair)
-            {
-                var hubConnection = await GetHubConnectionAsync();
-                await hubConnection.SendAsync("SetRSAPublicKey", RSAPublicKey, null, credentials);
-            }
+            await _gateway.AddEventCallbackAsync<IsUserExistDto>("UserExistanceResponse",
+                async isUserExistDTO =>
+                {
+                    _callbackExecutor.ExecuteSubscriptionsByName(isUserExistDTO, "UserExistanceResponse");
+                });
         }
 
         public async Task ActualizeConnectedUsersAsync()
         {
-            var hubConnection = await GetHubConnectionAsync();
-            await hubConnection.SendAsync("PushOnlineUsersToClients");
+            var gateway = _gateway ?? await ConfigureGateway();
+            await gateway.SendAsync("PushOnlineUsersToClients");
         }
 
         public async Task CheckIfUserOnline(string username)
         {
-            var hubConnection = await GetHubConnectionAsync();
-            await hubConnection.SendAsync("IsUserOnline", username);
+            var gateway = _gateway ?? await ConfigureGateway();
+            await gateway.SendAsync("IsUserOnline", username);
         }
 
         public async Task AddUserWebPushSubscription(NotificationSubscriptionDto subscriptionDTO)
         {
-            var hubConnection = await GetHubConnectionAsync();
-            await hubConnection.SendAsync("AddUserWebPushSubscription", subscriptionDTO);
+            var gateway = _gateway ?? await ConfigureGateway();
+            await gateway.SendAsync("AddUserWebPushSubscription", subscriptionDTO);
         }
 
         public async Task GetUserWebPushSubscriptions(CredentialsDTO credentialsDto)
         {
-            var hubConnection = await GetHubConnectionAsync();
-            await hubConnection.SendAsync("GetUserWebPushSubscriptions", credentialsDto);
+            var gateway = _gateway ?? await ConfigureGateway();
+            await gateway.SendAsync("GetUserWebPushSubscriptions", credentialsDto);
         }
 
         public async Task RemoveUserWebPushSubscriptions(NotificationSubscriptionDto[] subscriptionsToRemove)
@@ -247,14 +147,14 @@ namespace Ethachat.Client.Services.HubServices.HubServices.Implementations.Users
                 ($"At least one of parameters array " +
                  $"should have it's {nameof(NotificationSubscriptionDto.WebAuthnPair)} or {nameof(NotificationSubscriptionDto.JwtPair)} not null");
 
-            var hubConnection = await GetHubConnectionAsync();
-            await hubConnection.SendAsync("RemoveUserWebPushSubscriptions", subscriptionsToRemove);
+            var gateway = _gateway ?? await ConfigureGateway();
+            await gateway.SendAsync("RemoveUserWebPushSubscriptions", subscriptionsToRemove);
         }
 
         public async Task CheckIfUserExists(string username)
         {
-            var hubConnection = await GetHubConnectionAsync();
-            await hubConnection.SendAsync("CheckIfUserExist", username);
+            var gateway = _gateway ?? await ConfigureGateway();
+            await gateway.SendAsync("CheckIfUserExist", username);
         }
     }
 }

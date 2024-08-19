4da5cb135a5d53acaa47af2d364c77d1568c0128
Nikita <6stringsohei@gmail.com>
2024-08-19 19:22:24 +0300

    IUsersService.cs unused methods removed

--- src/Client/Presentation/Services/HubServices/HubServices/Implementations/UsersService/IUsersService.cs
+++ src/Client/Presentation/Services/HubServices/HubServices/Implementations/UsersService/IUsersService.cs
@@ -1,17 +1,11 @@
-﻿using EthachatShared.Encryption;
-using EthachatShared.Models.Authentication.Models.Credentials.CredentialsDTO;
+﻿using EthachatShared.Models.Authentication.Models.Credentials.CredentialsDTO;
 using EthachatShared.Models.WebPushNotification;
 
 namespace Ethachat.Client.Services.HubServices.HubServices.Implementations.UsersService
 {
     public interface IUsersService : IHubService
     {
-        Task SetRSAPublicKey(Key RSAPublicKey);
         Task ActualizeConnectedUsersAsync();
-        Guid SubscribeToConnectionIdReceived(Func<string, Task> callback);
-        void RemoveConnectionIdReceived(Guid subscriptionId);
-        Guid SubscribeToUsernameResolved(Func<string, Task> callback);
-        void RemoveUsernameResolved(Guid subscriptionId);
         Task CheckIfUserOnline(string username);
         Task AddUserWebPushSubscription(NotificationSubscriptionDto subscriptionDTO);
         Task GetUserWebPushSubscriptions(CredentialsDTO credentialsDto);

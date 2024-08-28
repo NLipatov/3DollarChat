1de15e835f2c9202af3023e994f1814395b82ae9
Nikita <6stringsohei@gmail.com>
2024-08-28 17:28:45 +0300

    fixed exception logging component; minor changes in main layout

--- src/Client/Presentation/Services/HubServices/HubServices/Implementations/ExceptionLoggingService/Implementation/LoggingService.cs
+++ src/Client/Presentation/Services/HubServices/HubServices/Implementations/ExceptionLoggingService/Implementation/LoggingService.cs
@@ -1,9 +1,5 @@
 using Client.Application.Gateway;
-using Client.Infrastructure.Gateway;
-using EthachatShared.Constants;
 using EthachatShared.Models.Logging.ExceptionLogging;
-using EthachatShared.Models.Message;
-using MessagePack;
 using Microsoft.AspNetCore.Components;
 using LogLevel = EthachatShared.Models.Logging.ExceptionLogging.LogLevel;
 
@@ -11,35 +7,18 @@ namespace Ethachat.Client.Services.HubServices.HubServices.Implementations.Excep
 
 public class LoggingService(NavigationManager navigationManager) : ILoggingService
 {
-    private IGateway? _gateway;
-
-    private async Task<IGateway> ConfigureGateway()
-    {
-        var gateway = new SignalrGateway();
-        await gateway.ConfigureAsync(navigationManager.ToAbsoluteUri(HubAddress.ExceptionLogging));
-        return gateway;
-    }
-
     public async Task LogException(Exception exception)
     {
-        var exceptionLog = new ExceptionLog
+        _ = new ExceptionLog
         {
             LogLevel = LogLevel.Error,
             Message = exception.Message,
             StackTrace = exception.StackTrace
         };
-
-        var gateway = await GetHubConnectionAsync();
-        await gateway.TransferAsync(new ClientToServerData
-        {
-            EventName = "Log",
-            Data = MessagePackSerializer.Serialize(exceptionLog),
-            Type = typeof(ExceptionLog)
-        });
     }
-    
-    public async Task<IGateway> GetHubConnectionAsync()
+
+    public Task<IGateway> GetHubConnectionAsync()
     {
-        return _gateway ??= await ConfigureGateway(); 
+        throw new NotImplementedException();
     }
 }

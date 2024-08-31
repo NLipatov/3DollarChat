69a69d9ecf60529c9b12036f642253edce7ffd0f
NLipatov <80638093+NLipatov@users.noreply.github.com>
2024-08-31 23:16:07 +0300

    Update CryptographyService.cs

--- src/Client/Infrastructure/Client.Infrastructure.Cryptography/CryptographyService.cs
+++ src/Client/Infrastructure/Client.Infrastructure.Cryptography/CryptographyService.cs
@@ -12,51 +12,22 @@ public class CryptographyService(IPlatformRuntime cryptographyExecutor) : ICrypt
     public Task<BinaryCryptogram> DecryptAsync<T>(BinaryCryptogram cryptogram, Key key)
         where T : ICryptoHandler
     {
-        var taskCompletionSource = new TaskCompletionSource<BinaryCryptogram>();
 
-        _cryptoTaskQueue.EnqueueTask(async () =>
-        {
-            try
-            {
-                var handler = (T?)Activator.CreateInstance(typeof(T), cryptographyExecutor);
-                if (handler is null)
-                    throw new NullReferenceException();
+        var handler = (T?)Activator.CreateInstance(typeof(T), cryptographyExecutor);
+        if (handler is null)
+            throw new NullReferenceException();
 
-                var result = await handler.Decrypt(cryptogram, key);
-                taskCompletionSource.SetResult(result);
-            }
-            catch (Exception ex)
-            {
-                taskCompletionSource.SetException(ex);
-            }
-        });
-
-        return taskCompletionSource.Task;
+        return await handler.Decrypt(cryptogram, key);
     }
 
     public Task<BinaryCryptogram> EncryptAsync<TCryptoHandler, TData>(TData data, Key key)
         where TCryptoHandler : ICryptoHandler
     {
-        var taskCompletionSource = new TaskCompletionSource<BinaryCryptogram>();
+        var handler = (TCryptoHandler?)Activator.CreateInstance(typeof(TCryptoHandler), cryptographyExecutor);
+        if (handler is null)
+            throw new NullReferenceException();
 
-        _cryptoTaskQueue.EnqueueTask(async () =>
-        {
-            try
-            {
-                var handler = (TCryptoHandler?)Activator.CreateInstance(typeof(TCryptoHandler), cryptographyExecutor);
-                if (handler is null)
-                    throw new NullReferenceException();
-
-                var result = await handler.Encrypt(data, key);
-                taskCompletionSource.SetResult(result);
-            }
-            catch (Exception ex)
-            {
-                taskCompletionSource.SetException(ex);
-            }
-        });
-
-        return taskCompletionSource.Task;
+        return await handler.Encrypt(data, key);
     }
 
     public async Task<Key> GenerateAesKeyAsync(string contact, string author)

bf843a7aa2762c91e64a40b0db81c5fcd07dfce9
Nikita <6stringsohei@gmail.com>
2024-09-01 15:41:45 +0300

    fixed CryptographyService.cs not generating RSA keys

--- src/Client/Infrastructure/Client.Infrastructure.Cryptography/CryptographyService.cs
+++ src/Client/Infrastructure/Client.Infrastructure.Cryptography/CryptographyService.cs
@@ -1,19 +1,28 @@
 ﻿using Client.Application.Cryptography;
+using Client.Application.Cryptography.KeyStorage;
 using Client.Application.Runtime;
 using EthachatShared.Encryption;
 using EthachatShared.Models.Cryptograms;
 
 namespace Client.Infrastructure.Cryptography;
 
-public class CryptographyService(IPlatformRuntime cryptographyExecutor) : ICryptographyService
+public class CryptographyService : ICryptographyService
 {
+    private readonly IPlatformRuntime _platformRuntime;
+    private readonly IKeyStorage _keyStorage;
     private readonly CryptoTaskQueue _cryptoTaskQueue = new();
+
+    public CryptographyService(IPlatformRuntime platformRuntime, IKeyStorage keyStorage)
+    {
+        _platformRuntime = platformRuntime;
+        _keyStorage = keyStorage;
+        _ = GenerateRsaKeyPairAsync();
+    }
 
     public async Task<BinaryCryptogram> DecryptAsync<T>(BinaryCryptogram cryptogram, Key key)
         where T : ICryptoHandler
     {
-
-        var handler = (T?)Activator.CreateInstance(typeof(T), cryptographyExecutor);
+        var handler = (T?)Activator.CreateInstance(typeof(T), _platformRuntime);
         if (handler is null)
             throw new NullReferenceException();
 
@@ -23,7 +32,7 @@ public class CryptographyService(IPlatformRuntime cryptographyExecutor) : ICrypt
     public async Task<BinaryCryptogram> EncryptAsync<TCryptoHandler, TData>(TData data, Key key)
         where TCryptoHandler : ICryptoHandler
     {
-        var handler = (TCryptoHandler?)Activator.CreateInstance(typeof(TCryptoHandler), cryptographyExecutor);
+        var handler = (TCryptoHandler?)Activator.CreateInstance(typeof(TCryptoHandler), _platformRuntime);
         if (handler is null)
             throw new NullReferenceException();
 
@@ -32,7 +41,7 @@ public class CryptographyService(IPlatformRuntime cryptographyExecutor) : ICrypt
 
     public async Task<Key> GenerateAesKeyAsync(string contact, string author)
     {
-        var key = await cryptographyExecutor.InvokeAsync<string>("GenerateAESKeyAsync", []);
+        var key = await _platformRuntime.InvokeAsync<string>("GenerateAESKeyAsync", []);
         return new Key
         {
             Id = Guid.NewGuid(),
@@ -44,5 +53,28 @@ public class CryptographyService(IPlatformRuntime cryptographyExecutor) : ICrypt
             CreationDate = DateTime.UtcNow,
             IsAccepted = false
         };
+    }
+
+    private async Task GenerateRsaKeyPairAsync()
+    {
+        var keyPair = await _platformRuntime.InvokeAsync<string[]>("GenerateRSAOAEPKeyPairAsync", []);
+        var publicRsa = new Key
+        {
+            Id = Guid.NewGuid(),
+            Value = keyPair[0],
+            Format = KeyFormat.PemSpki,
+            Type = KeyType.RsaPublic,
+            Contact = string.Empty
+        };
+        var privateRsa = new Key
+        {
+            Id = Guid.NewGuid(),
+            Value = keyPair[1],
+            Format = KeyFormat.PemSpki,
+            Type = KeyType.RsaPrivate,
+            Contact = string.Empty
+        };
+        await _keyStorage.StoreAsync(privateRsa);
+        await _keyStorage.StoreAsync(publicRsa);
     }
 }

a44acceb2542c5bf377bc71d8d04bcae753d9224
Nikita <6stringsohei@gmail.com>
2024-12-17 17:59:55 +0300

    code cleanup

--- src/Server/Ethachat.Server/Utilities/UsernameResolver/UsernameResolverService.cs
+++ src/Server/Ethachat.Server/Utilities/UsernameResolver/UsernameResolverService.cs
@@ -1,38 +1,11 @@
-﻿using System.IdentityModel.Tokens.Jwt;
-using Ethachat.Client.Services.Authentication;
-using Ethachat.Server.Utilities.HttpMessaging;
+﻿using Ethachat.Server.Utilities.HttpMessaging;
 using EthachatShared.Models.Authentication.Models;
 using EthachatShared.Models.Authentication.Models.Credentials.CredentialsDTO;
 
 namespace Ethachat.Server.Utilities.UsernameResolver;
 
-public class UsernameResolverService : IUsernameResolverService
+public class UsernameResolverService(IServerHttpClient serverHttpClient) : IUsernameResolverService
 {
-    private readonly IServerHttpClient _serverHttpClient;
-    private readonly IAuthenticationManager _authenticationManager;
-
-    public UsernameResolverService(IServerHttpClient serverHttpClient)
-    {
-        _serverHttpClient = serverHttpClient;
-    }
-
     public async Task<AuthResult> GetUsernameAsync(CredentialsDTO credentialsDto)
-    {
-        var result = await _serverHttpClient.GetUsernameByCredentials(credentialsDto);
-        return result;
-    }
-
-    private bool IsTokenReadable(string accessToken)
-    {
-        var tokenHandler = new JwtSecurityTokenHandler();
-        try
-        {
-            tokenHandler.ReadJwtToken(accessToken);
-            return true;
-        }
-        catch (Exception e)
-        {
-            return false;
-        }
-    }
+        => await serverHttpClient.GetUsernameByCredentials(credentialsDto);
 }
